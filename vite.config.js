e5fc755535f2a5018fa9ae99eb66cc5a83cc1138
Boryslav Mytrofanov <borispion@gmail.com>
2026-02-03 18:45:57 +0000

    Set base URL to /todolist and update asset paths accordingly

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => ({
-  base: "/",
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  test: {
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-  },
-  css: {
-    modules: {
-      localsConvention: "camelCaseOnly",
+export default defineConfig(({ command }) => {
+  console.log("command", command);
+  return {
+    base: "/todolist",
+    plugins: [react()],
+    build: {
+      outDir: "dist",
     },
-  },
-}));
+    test: {
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.ts",
+    },
+    css: {
+      modules: {
+        localsConvention: "camelCaseOnly",
+      },
+    },
+  };
+});
