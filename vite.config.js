8eb9613ac011ff3150d12bdd92bf8cec77b3ad58
Boryslav Mytrofanov <borispion@gmail.com>
2026-02-03 18:09:01 +0000

    Fix manifest syntax error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/todolist/",
+export default defineConfig(({ command }) => ({
+  base: command === "serve" ? "/" : "/todolist/",
   plugins: [react()],
   build: {
     outDir: "dist",
@@ -16,4 +16,4 @@ export default defineConfig({
       localsConvention: "camelCaseOnly",
     },
   },
-});
+}));
