5b597bf22c934de14a3331cad496749c28f933d1
Boryslav Mytrofanov <borispion@gmail.com>
2026-02-04 02:42:18 +0000

    Update favicons and manifests, fix asset paths, and adjust base URL for build command

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ command }) => {
-  console.log("command", command);
   return {
-    base: "/todolist",
+    base: command === "build" ? "/todolist/" : "/",
     plugins: [react()],
     build: {
       outDir: "dist",
